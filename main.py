dfdf740466715ddec38fef3de17f78310612e480
Kenneth Gulbrandsøy <kenneth@discoos.org>
2025-06-02 16:28:49 +0200

    Improved the design

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import os
+import uuid
 import json
 import zipfile
 
+from datetime import datetime
 from werkzeug.utils import secure_filename
-from flask import Flask, request, jsonify, render_template, send_file
+from flask import Flask, request, redirect, url_for, flash, jsonify, render_template, send_file
+from flask_session import Session
 
 from sartopo2faks import classify_features
 
 app = Flask(__name__)
+sess = Session()
 
 UPLOAD_FOLDER = 'uploads'
 OUTPUT_FOLDER = 'output'
@@ -25,29 +29,31 @@ def home():
     # Render the upload form
     return render_template('index.html')
 
+@app.route('/process', methods=['POST'])
+def process():
+    # Secure the filename and save it to UPLOAD_FOLDER
+    upload_path = upload()
 
-@app.route('/select', methods=['POST'])
-def list_features():
-    # Get the list of features from the uploaded GeoJSON file
+    # Determine the action (either 'select' or 'convert')
+    action = request.form.get('action')
+    if action == 'select':
+        return list_features(upload_path)
+    elif action == 'convert':
+        return convert(upload_path)
+    else:
+        flash('Invalid action specified!', 'error')
+        return redirect(request.url)
+
+# Get the list of features from the uploaded GeoJSON file
+def list_features(upload_path):
     try:
-        # Handle file upload
-        if 'geojson_file' not in request.files:
-            return jsonify({"error": "No file part"}), 400
-
-        file = request.files['geojson_file']
-        if file.filename == '':
-            return jsonify({"error": "No selected file"}), 400
-
-        # Save the uploaded GeoJSON file
-        filename = secure_filename(file.filename)
-        geojson_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(geojson_path)
-
-        with open(geojson_path, 'r') as file:
+        # Open uploaded GeoJSON file
+        with open(upload_path, 'r') as file:
             geojson_data = json.load(file)
             features = geojson_data.get('features', [])
 
-        # Create a dictionary to map folderId to folder names (for 'Folder' features)
+        # Create a dictionary to map folderId to folder
+        # names (for 'Folder' features)
         folder_mapping = {}
         for feature in features:
             folder_id = feature.get('id')
@@ -72,81 +78,46 @@ def list_features():
                     'folder': folder_name
                 })
 
-
         return render_template('select.html',
             features=feature_list,
-            file_path=geojson_path
+            upload_path=upload_path
         )
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@app.route('/export', methods=['POST'])
-def export_features():
+def upload():
+    # Check if the post request has the file part
+    if 'geojson_file' not in request.files:
+        flash('No file uploaded!', 'error')
+        return redirect(request.url)
+
+    file = request.files['geojson_file']
+
+    # Check if the file is selected
+    if file.filename == '':
+        flash('No selected file!', 'error')
+        return redirect(request.url)
+
+    # Secure the filename and save it to UPLOAD_FOLDER
+    filename = secure_filename(file.filename)
+
+    # Generate a unique folder name using a UUID
+    unique_folder = f"upload_{uuid.uuid4()}"
+    upload_folder = os.path.join(UPLOAD_FOLDER, unique_folder)
+
+    # Ensure the unique upload folder exists (create it if necessary)
+    os.makedirs(upload_folder, exist_ok=True)
+
+    upload_path = os.path.join(upload_folder, filename)
+    file.save(upload_path)
+
+    return upload_path
+
+def convert(upload_path):
     try:
-        selected_ids = request.form.getlist('features')  # Extract selected feature IDs
-        geojson_path = request.form.get('geojson_path')
-
-        # Load original GeoJSON data
-        with open(geojson_path, 'r') as file:
-            geojson_data = json.load(file)
-
-        # Filter features based on user selection
-        filtered_features = [geojson_data['features'][int(i)] for i in selected_ids]
-        source_data = {
-            "features": filtered_features,
-        }
-
-        # Classify the enriched features
-        sink_path = os.path.join(app.config['OUTPUT_FOLDER'], "sink_files")
-        classify_features(source_data, sink_path)
-
-        processed_files = [os.path.join(sink_path, file) for file in os.listdir(sink_path)
-                           if os.path.isfile(os.path.join(sink_path, file))]
-
-        upload_name = os.path.splitext(os.path.basename(geojson_path))[0]
-
-        # Zip all files in the sink folder
-        zip_path = os.path.join(app.config['OUTPUT_FOLDER'], upload_name+".zip")
-        with zipfile.ZipFile(zip_path, 'w') as zipf:
-            for file in processed_files:
-                zipf.write(file, os.path.basename(file))
-
-        # Return the zip file to the client for download
-        return send_file(zip_path, as_attachment=True)
-
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
-
-@app.route('/process', methods=['POST'])
-def process_file():
-    try:
-        # Handle file upload
-        if 'geojson_file' not in request.files:
-            return jsonify({"error": "No file part"}), 400
-
-        file = request.files['geojson_file']
-        if file.filename == '':
-            return jsonify({"error": "No selected file"}), 400
-
-        # TODO: Check if files exists in uploads
-        filename = secure_filename(file.filename)
-        geojson_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(geojson_path)
-
-        print("GeoJSON Path (Relative):", geojson_path)
-        print("GeoJSON Path (Absolute):", os.path.abspath(geojson_path))
-        return jsonify({
-            "GeoJSON Path (Relative):": geojson_path,
-            "GeoJSON Path (Absolute):": os.path.abspath(geojson_path),
-        }), 500
-
-        if not os.path.exists(geojson_path):
-            return jsonify({"error": f"{geojson_path} not found"}), 500
-
         # Open the uploaded file and parse it as JSON
-        with open(geojson_path, "r") as source_file:
+        with open(upload_path, "r") as source_file:
             source_data = json.load(source_file)  # Parse JSON from file
 
         # Classify the enriched features
@@ -156,7 +127,7 @@ def process_file():
         processed_files = [os.path.join(sink_path, file) for file in os.listdir(sink_path)
             if os.path.isfile(os.path.join(sink_path, file))]
 
-        upload_name = os.path.splitext(os.path.basename(filename))[0]
+        upload_name = os.path.splitext(os.path.basename(upload_path))[0]
 
         # Zip all files in the sink folder
         zip_path = os.path.join(app.config['OUTPUT_FOLDER'], upload_name+".zip")
@@ -168,9 +139,59 @@ def process_file():
         return send_file(zip_path, as_attachment=True)
 
     except Exception as e:
-        return jsonify({"error": str(e)}), 500
+        flash(f"Error while converting file: {str(e)}", 'error')
+        return redirect(request.url)
 
+@app.route('/export', methods=['POST'])
+def export():
+    try:
+        selected_ids = request.form.getlist('features')  # Extract selected feature IDs
+        upload_path = request.form.get('upload_path')
 
+        print(f"export: upload_path = {upload_path}, type = {type(upload_path)}")
+
+        # Load original GeoJSON data
+        with open(upload_path, 'r') as file:
+            geojson_data = json.load(file)
+
+        # Filter features based on user selection
+        filtered_features = [geojson_data['features'][int(i)] for i in selected_ids]
+        source_data = { "features": filtered_features }
+
+        # Generate a unique folder name using a UUID
+        unique_folder = f"job_{uuid.uuid4()}"
+
+        # Create the unique folder path
+        sink_path = os.path.join(app.config['OUTPUT_FOLDER'], unique_folder)
+        classify_features(source_data, sink_path)
+
+        processed_files = [os.path.join(sink_path, file) for file in os.listdir(sink_path)
+                           if os.path.isfile(os.path.join(sink_path, file))]
+
+        upload_name = os.path.splitext(os.path.basename(upload_path))[0]
+        zip_path = os.path.join(
+            app.config['OUTPUT_FOLDER'], unique_folder, f"{upload_name}.zip"
+        )
+
+        # Zip all files in the sink folder
+        with zipfile.ZipFile(zip_path, 'w') as zipf:
+            for file in processed_files:
+                zipf.write(file, os.path.basename(file))
+
+        # Return the zip file to the client for download
+        return send_file(zip_path, as_attachment=True)
+
+    except Exception as e:
+        flash(f"Error while exporting file: {str(e)}", 'error')
+        return redirect(request.url)
 
 if __name__ == "__main__":
+    # Quick test configuration. Please use proper Flask configuration options
+    # in production settings, and use a separate file or environment variables
+    # to manage the secret key!
+    app.secret_key = 'some_secret_key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+    sess.init_app(app)
+
     app.run(host="0.0.0.0")
